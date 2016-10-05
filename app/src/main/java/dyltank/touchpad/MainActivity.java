3ef3126661c7d1fda708eb889628a62032e7e883
Dylange <dylan_lange@live.com>
2016-10-06 02:38:06 +1300

    successfully sends packets to server of mouse movements

--- app/src/main/java/dyltank/touchpad/MainActivity.java
+++ app/src/main/java/dyltank/touchpad/MainActivity.java
@@ -1,6 +1,7 @@
 package dyltank.touchpad;
 
 import android.media.Image;
+import android.os.AsyncTask;
 import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,17 +10,47 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
 
     private float initialX, initialY, lastX, lastY;
-    private String TAG = "TOUCH"
-;
+    public float dx, dy;
+
+    private String TAG = "TOUCH";
+    private AsyncTask networkClient;
+
+    public DatagramSocket clientSocket;
+    public InetAddress IPAddress;
+
+    public String message = "";
+    private DecimalFormat df = new DecimalFormat("#.#####");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView panel = (ImageView)findViewById(R.id.imageView);
+        //ImageView panel = (ImageView)findViewById(R.id.imageView);
+        networkClient = new AsyncNetworkClient().execute();
+
+    }
+
+    private void move(int numFingers, float dx, float dy) throws IOException{
+        //System.out.println(numFingers + " finger move -> X:" + dx + " | Y:" + dy);
+
+        //message = String.format("%d:%f:%f", numFingers, dx, dy);
+
+        this.dx += dx;
+        this.dy += dy;
+        df.format(this.dx);
+        df.format(this.dy);
 
     }
 
@@ -39,7 +70,16 @@ public class MainActivity extends AppCompatActivity {
             case MotionEvent.ACTION_MOVE:
                 float newX = event.getX();
                 float newY = event.getY();
-                Log.d(TAG, "Action was MOVE");
+
+                try{
+                    move(event.getPointerCount(), newX - lastX, newY - lastY);
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+                lastX = newX;
+                lastY = newY;
+
+                //Log.d(TAG, "Action was MOVE");
                 break;
 
             case MotionEvent.ACTION_UP:
@@ -91,4 +131,44 @@ public class MainActivity extends AppCompatActivity {
         }
         return "";
     }
+
+    class AsyncNetworkClient extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute(){
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Log.d("NETWORKCLIENT", "Connection started");
+
+            try{
+                clientSocket = new DatagramSocket();
+                IPAddress = InetAddress.getByName("192.168.1.70");
+
+                while(true){
+                    if(dx == 0 && dy == 0) continue;
+                    byte[] sendData = (dx + ":" + dy).getBytes();
+                    dx = dy = 0;
+                    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 6969);
+
+                    clientSocket.send(sendPacket);
+                }
+
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+
+            clientSocket.close();
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result){
+
+        }
+
+    }
+
 }
